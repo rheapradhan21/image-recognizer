5e8127f29dabc30af3d4403547526f3104c6b17b
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2023-11-17 14:10:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,23 @@ function take_snapshot(){
 console.log('ml5 version', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/o6o2DswGT/model.json', modelloaded);
+
+function modelloaded(){
+    console.log("model is loaded");
+}
+
+function check(){
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('object_name').innerHTML = results[0].label;
+        document.getElementById('object_accuracy').innerHTML = results[0].confidence.toFixed(2);
+    }
+}
